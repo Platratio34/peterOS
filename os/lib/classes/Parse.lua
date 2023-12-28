1547f22977973915f4b7d78201e0a0dccb6de3e9
Peter <pcrall04@gmail.com>
2023-12-27 22:10:21 -0800

    Added Parser class

---
+++ os/lib/classes/Parse.lua
@@ -0,0 +1,76 @@
+local clParse = {}
+_G.pos.clParse = clParse
+
+---@class Parser Command line parser utility
+---@field flags table<string,CLFlag> Table of command line flags
+local Parser = {
+    flags = {}
+}
+
+---Create a new parser
+---@return Parser
+function clParse.Parser()
+    local o = {}
+    setmetatable(o, { __index = Parser })
+    return o
+end
+
+function Parser:parse(args)
+    local cFlags = {}
+    local cArgs = {}
+
+    local temp = nil
+
+    for _,arg in pairs(args) do
+        if temp then
+            if arg:sub(-1) == '"' then
+                cArgs:insert(temp..arg:sub(1,-2))
+                temp = nil
+            else
+                temp = temp .. arg
+            end
+        elseif arg:sub(1, 1) == '-' then
+            local name = arg:sub(2)
+            local val = true ---@type any
+            if arg:cont('=') then
+                local i = arg:find('=')
+                name = arg:sub(name, i - 1)
+                val = arg:sub(i + 1)
+                if val == 'true' then
+                    val = true
+                elseif val == 'false' then
+                    val = false
+                elseif tonumber(val) then
+                    val = tonumber(val)
+                end
+            end
+            if self.flags[name] then
+                name = self.flags[name].name
+            end
+            cFlags[name] = val
+        elseif arg:sub(1,1) == '"' then
+            temp = arg:sub(2)
+        else
+            cArgs:insert(arg)
+        end
+    end
+
+    return cArgs, cFlags
+end
+
+function Parser:addFlag(name, short)
+    local flag = {
+        name = name,
+        short = short
+    }
+    self.flags[name] = flag
+    self.flags[short] = flag
+end
+
+function clParse.parse(args)
+    return clParse.Parser():parse(args)
+end
+
+---@class CLFlag
+---@field name string
+---@field short nil|string
